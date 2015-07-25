5fd65f1298faafd4bf922fb37b7e81f53ad881f7
St. Laurent, Daryl <daryl.stlaurent@stresearch.com>
2015-07-25 07:42:26 -0400

    Markdown editor work in progress

--- application.js
+++ application.js
@@ -1,10 +1,305 @@
-// regex to match latex blocks
-var LATEX_REGEX = /(?:\\\\\(([^]*?)\\\\\))|(?:\$\$([^]*?)\$\$)/g;
+////////////////////////////
+// CODEMIRROR SIMPLE MODE //
+////////////////////////////
+
+// CodeMirror, copyright (c) by Marijn Haverbeke and others
+// Distributed under an MIT license: http://codemirror.net/LICENSE
+
+(function(mod) {
+  if (typeof exports == "object" && typeof module == "object") // CommonJS
+    mod(require("../../lib/codemirror"));
+  else if (typeof define == "function" && define.amd) // AMD
+    define(["../../lib/codemirror"], mod);
+  else // Plain browser env
+    mod(CodeMirror);
+})(function(CodeMirror) {
+  "use strict";
+
+  CodeMirror.defineSimpleMode = function(name, states) {
+    CodeMirror.defineMode(name, function(config) {
+      return CodeMirror.simpleMode(config, states);
+    });
+  };
+
+  CodeMirror.simpleMode = function(config, states) {
+    ensureState(states, "start");
+    var states_ = {}, meta = states.meta || {}, hasIndentation = false;
+    for (var state in states) if (state != meta && states.hasOwnProperty(state)) {
+      var list = states_[state] = [], orig = states[state];
+      for (var i = 0; i < orig.length; i++) {
+        var data = orig[i];
+        list.push(new Rule(data, states));
+        if (data.indent || data.dedent) hasIndentation = true;
+      }
+    }
+    var mode = {
+      startState: function() {
+        return {state: "start", pending: null,
+                local: null, localState: null,
+                indent: hasIndentation ? [] : null};
+      },
+      copyState: function(state) {
+        var s = {state: state.state, pending: state.pending,
+                 local: state.local, localState: null,
+                 indent: state.indent && state.indent.slice(0)};
+        if (state.localState)
+          s.localState = CodeMirror.copyState(state.local.mode, state.localState);
+        if (state.stack)
+          s.stack = state.stack.slice(0);
+        for (var pers = state.persistentStates; pers; pers = pers.next)
+          s.persistentStates = {mode: pers.mode,
+                                spec: pers.spec,
+                                state: pers.state == state.localState ? s.localState : CodeMirror.copyState(pers.mode, pers.state),
+                                next: s.persistentStates};
+        return s;
+      },
+      token: tokenFunction(states_, config),
+      innerMode: function(state) { return state.local && {mode: state.local.mode, state: state.localState}; },
+      indent: indentFunction(states_, meta)
+    };
+    if (meta) for (var prop in meta) if (meta.hasOwnProperty(prop))
+      mode[prop] = meta[prop];
+    return mode;
+  };
+
+  function ensureState(states, name) {
+    if (!states.hasOwnProperty(name))
+      throw new Error("Undefined state " + name + "in simple mode");
+  }
+
+  function toRegex(val, caret) {
+    if (!val) return /(?:)/;
+    var flags = "";
+    if (val instanceof RegExp) {
+      if (val.ignoreCase) flags = "i";
+      val = val.source;
+    } else {
+      val = String(val);
+    }
+    return new RegExp((caret === false ? "" : "^") + "(?:" + val + ")", flags);
+  }
+
+  function asToken(val) {
+    if (!val) return null;
+    if (typeof val == "string") return val.replace(/\./g, " ");
+    var result = [];
+    for (var i = 0; i < val.length; i++)
+      result.push(val[i] && val[i].replace(/\./g, " "));
+    return result;
+  }
+
+  function Rule(data, states) {
+    if (data.next || data.push) ensureState(states, data.next || data.push);
+    this.regex = toRegex(data.regex);
+    this.token = asToken(data.token);
+    this.data = data;
+  }
+
+  function tokenFunction(states, config) {
+    return function(stream, state) {
+      if (state.pending) {
+        var pend = state.pending.shift();
+        if (state.pending.length == 0) state.pending = null;
+        stream.pos += pend.text.length;
+        return pend.token;
+      }
+
+      if (state.local) {
+        if (state.local.end && stream.match(state.local.end)) {
+          var tok = state.local.endToken || null;
+          state.local = state.localState = null;
+          return tok;
+        } else {
+          var tok = state.local.mode.token(stream, state.localState), m;
+          if (state.local.endScan && (m = state.local.endScan.exec(stream.current())))
+            stream.pos = stream.start + m.index;
+          return tok;
+        }
+      }
+
+      var curState = states[state.state];
+      for (var i = 0; i < curState.length; i++) {
+        var rule = curState[i];
+        var matches = (!rule.data.sol || stream.sol()) && stream.match(rule.regex);
+        if (matches) {
+          if (rule.data.next) {
+            state.state = rule.data.next;
+          } else if (rule.data.push) {
+            (state.stack || (state.stack = [])).push(state.state);
+            state.state = rule.data.push;
+          } else if (rule.data.pop && state.stack && state.stack.length) {
+            state.state = state.stack.pop();
+          }
+
+          if (rule.data.mode)
+            enterLocalMode(config, state, rule.data.mode, rule.token);
+          if (rule.data.indent)
+            state.indent.push(stream.indentation() + config.indentUnit);
+          if (rule.data.dedent)
+            state.indent.pop();
+          if (matches.length > 2) {
+            state.pending = [];
+            for (var j = 2; j < matches.length; j++)
+              if (matches[j])
+                state.pending.push({text: matches[j], token: rule.token[j - 1]});
+            stream.backUp(matches[0].length - (matches[1] ? matches[1].length : 0));
+            return rule.token[0];
+          } else if (rule.token && rule.token.join) {
+            return rule.token[0];
+          } else {
+            return rule.token;
+          }
+        }
+      }
+      stream.next();
+      return null;
+    };
+  }
+
+  function cmp(a, b) {
+    if (a === b) return true;
+    if (!a || typeof a != "object" || !b || typeof b != "object") return false;
+    var props = 0;
+    for (var prop in a) if (a.hasOwnProperty(prop)) {
+      if (!b.hasOwnProperty(prop) || !cmp(a[prop], b[prop])) return false;
+      props++;
+    }
+    for (var prop in b) if (b.hasOwnProperty(prop)) props--;
+    return props == 0;
+  }
+
+  function enterLocalMode(config, state, spec, token) {
+    var pers;
+    if (spec.persistent) for (var p = state.persistentStates; p && !pers; p = p.next)
+      if (spec.spec ? cmp(spec.spec, p.spec) : spec.mode == p.mode) pers = p;
+    var mode = pers ? pers.mode : spec.mode || CodeMirror.getMode(config, spec.spec);
+    var lState = pers ? pers.state : CodeMirror.startState(mode);
+    if (spec.persistent && !pers)
+      state.persistentStates = {mode: mode, spec: spec.spec, state: lState, next: state.persistentStates};
+
+    state.localState = lState;
+    state.local = {mode: mode,
+                   end: spec.end && toRegex(spec.end),
+                   endScan: spec.end && spec.forceEnd !== false && toRegex(spec.end, false),
+                   endToken: token && token.join ? token[token.length - 1] : token};
+  }
+
+  function indexOf(val, arr) {
+    for (var i = 0; i < arr.length; i++) if (arr[i] === val) return true;
+  }
+
+  function indentFunction(states, meta) {
+    return function(state, textAfter, line) {
+      if (state.local && state.local.mode.indent)
+        return state.local.mode.indent(state.localState, textAfter, line);
+      if (state.indent == null || state.local || meta.dontIndentStates && indexOf(state.state, meta.dontIndentStates) > -1)
+        return CodeMirror.Pass;
+
+      var pos = state.indent.length - 1, rules = states[state.state];
+      scan: for (;;) {
+        for (var i = 0; i < rules.length; i++) {
+          var rule = rules[i];
+          if (rule.data.dedent && rule.data.dedentIfLineStart !== false) {
+            var m = rule.regex.exec(textAfter);
+            if (m && m[0]) {
+              pos--;
+              if (rule.next || rule.push) rules = states[rule.next || rule.push];
+              textAfter = textAfter.slice(m[0].length);
+              continue scan;
+            }
+          }
+        }
+        break;
+      }
+      return pos < 0 ? 0 : state.indent[pos];
+    };
+  }
+});
+
+
+/////////////////////////////////////
+// REGEX HANDLING WORK IN PROGRESS //
+/////////////////////////////////////
+
+var regexCombine = function(){ // merge a list of regexes into a single regex
+  return new RegExp( _.map(arguments,function(x){return x.source}).join('') );
+}
+var regexReplace = function(r, obj){
+  var src = r.source;
+  _.each(obj, function(v,k){
+    src = src.replace(new RegExp(k,'g'), v.source);
+  });
+  return new RegExp(src);
+}
+var regexNonCapture = function(){
+  return new RegExp( '(?:' + _.map(arguments,function(x){return x.source}).join('') + ')');
+}
+var listRegex = regexReplace(regexCombine(
+  /^( *)/               ,// Initial space
+  /(bull) /             ,// Bullet character
+  /[\s\S]+?/            ,// Minimal multi-line wildcard
+  regexNonCapture(
+    /hr|def|\n{2,}/     ,//
+    /(?! )/             ,//
+    /(?!\1bull )/       ,//
+    /\n*|\s*$/           //
+  )
+), {
+  bull:  /(?:[*+-]|\d+\.)/  // *, +, -, or xx. (non-capture)
+});
+console.log(listRegex);
+
+
+///////////////////////////////////////
+// EXTENDED MARKDOWN CODEMIRROR MODE //
+///////////////////////////////////////
+
+// RESOURCES
+//   markdown syntax: http://daringfireball.net/projects/markdown/basics
+//   markdown basics: https://help.github.com/articles/markdown-basics/
+//   GFM basics: https://help.github.com/articles/github-flavored-markdown/
+//   javascript regex: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions
+//   codemirror manual: https://codemirror.net/doc/manual.html
+//   codemirror simple mode demo: https://codemirror.net/demo/simplemode.html
+//   codemirror markdown mode demo: http://codemirror.net/mode/markdown/
+//   regex tester: https://regex101.com/#javascript
+
+// Define a CodeMirror mode for markdown editor
+CodeMirror.defineSimpleMode("gfm", {
+  start: [
+    { regex:/```/, token: 'comment', next:'code' },
+    { regex:/   .+/, token:'comment', sol:true },
+    { regex:/`.+`/, token:'comment'},
+    { regex:/ *#+.*/, token:'header', sol:true },
+    { regex:/ *\*.*/, token:'variable-2', sol:true },
+    { regex:/ *\d+\. .*/, token:'variable-2', sol:true },
+    { regex:/\*{2}.+?\*{2}/, token:'strong'},
+    { regex:/\b_.+?_\b/, token:'em'},
+    { regex:/\\\\\(.+?\\\\\)/, token:'error'},
+    { regex:/(=|\-)+/, token:'header', sol:true},
+
+    { regex:/!?\[((?:\[[^\]]*\]|[^\[\]]|\](?=[^\[]*\]))*)\]\(\s*<?([\s\S]*?)>?(?:\s+['"]([\s\S]*?)['"])?\s*\)/, token:['link','string']}
+
+    //{ regex:/\b_(?:__|[\s\S])+?_\b/, token:'em' },
+    //{ regex:/\b\*\*/, token:'strong', push:'strong' }
+  ],
+  code: [
+    {regex:/.*?```/, token: 'comment', next:'start'},
+    {regex:/.*/, token:'comment'}
+  ],
+  strong: [
+    { regex:/.*\*{2}\b}/, token: 'strong', pop:true },
+    { regex:/.*/, token:'string'}
+  ]
+});
 
 
 ////////////////////////////
 // CACHED LATEX RENDERING //
 ////////////////////////////
+
+// regex to match latex blocks
+var LATEX_REGEX = /(?:\\\\\(([^]*?)\\\\\))|(?:\$\$([^]*?)\$\$)/g;
 
 // hash table of processed latex
 var latexMap = {};
@@ -48,13 +343,13 @@ renderMarkdown = function(x, $el) {
   // populate specified element with text converted to markdown
   $el.html(marked(x
     .replace(/\r?\n/g,    '\n'         ) // standardize line breaks to /n
-    .replace(LATEX_REGEX, '<latex />'  ) // escape latex
+    //.replace(LATEX_REGEX, '<latex />'  ) // escape latex
     .replace(/\[TOC\]/gi, '<toc></toc>') // TOC jQuery can find
-  ));
+  ,{lineMarkers:true}));
 
   // return latex to page
-  $el.find('latex').each(function(){
-    $(this).text(latex.shift());
+  //$el.find('latex').each(function(){
+    //$(this).text(latex.shift());
     /*var tex = latex.shift().replace(/^\\\\\(/, '').replace(/\\\\\)$/, '').replace(/^\$\$/, '').replace(/\$\$$/, '');
     try {
       tex = katex.renderToString(tex);
@@ -62,7 +357,7 @@ renderMarkdown = function(x, $el) {
       console.log("Error processing latex: \n\n" + tex + "\n\n" + err);
     }
     $(this).html(tex);*/
-  });
+  //});
 
   // create a table of contents
   var toc = marked(
@@ -101,3 +396,132 @@ renderMarkdown = function(x, $el) {
   };
 
 }
+
+
+/////////////////////////
+// CODEMIRROR HANDLING //
+/////////////////////////
+
+// interpolate data to a linear range
+interpolate = function(x_vec, y_vec, xi, xf) {
+  var out = [], x1, x2, y1, y2, m;
+  var updateSlope = function(){
+    x1 = x2; x2 = x_vec.shift();
+    y1 = y2; y2 = y_vec.shift();
+    m = (y2-y1)/(x2-x1);
+  }
+  updateSlope(); updateSlope(); // grab first 2 points
+  for (var x = xi; x < xf; x++) {
+    if (x > x2 && x_vec.length > 0) {
+      updateSlope();  // update slope if outside range and still have data
+    }
+    out.push(y1 + m*(x-x1)); // add interpolated point to output array
+  }
+  return out;
+}
+
+// return locations of lines in editor window
+visibleLines = function(cm){
+  var scrollInfo = cm.getScrollInfo();
+  var topLine    = cm.lineAtHeight(scrollInfo.top                          , 'local');
+  var bottomLine = cm.lineAtHeight(scrollInfo.top + scrollInfo.clientHeight, 'local');
+  var maxLine    = cm.lineCount() - 1;
+  return {
+    top:    topLine,
+    bottom: Math.min(maxLine, bottomLine),
+    cursor: Math.min(maxLine, cm.getCursor().line)
+  }
+}
+
+// return editor contents with line markers
+contentWithMarkers = function(cm){
+  var data = cm.getValue().split("\n");
+  for (var i = 0; i < data.length; i++) {
+    data[i] += "<codemirror-line" + i + " />"
+  }
+  var tok = marked.lexer(data.join("\n"));
+  var out_tok = [];
+  for (var i = 0; i < tok.length; i++) {
+    if (tok[i].text != null && tok[i].text.match(/<codemirror-(top|cursor|bottom)\/>/)) {
+      console.log(tok[i]);
+    }
+  }
+  debugger
+}
+
+// line number lookup array
+var lineMap;
+
+// scroll to the specified line number
+var scrollTo = function(line, marker) {
+  if (marker == "bottom") {
+    $('#viewer-container').scrollTop(lineMap[line-1] - $('#viewer-container').height());
+  } else if (marker == "center") {
+    $('#viewer-container').scrollTop(lineMap[line-1] - $('#viewer-container').height()*0.5);
+  } else {
+    $('#viewer-container').scrollTop(lineMap[line-1]);
+  }
+}
+
+// function to render markdown
+var render = function(){
+
+  // capture any existing rendered latex
+  $('#viewer latex').each(function(){ captureLatex($(this)); });
+
+  // execute rendering
+  renderMarkdown(cm.getValue(),$('#viewer'));
+
+  // capture line numbers
+  var x = [], y = [];
+  var lineRefs = $('span.linemarker').map( function(){
+    x.push(parseInt($(this).text()));
+    y.push($(this).next().position().top);
+  })
+
+  // interpolate/extrapolate to create a line number lookup array
+  lineMap = interpolate(x, y, 1, cm.lastLine());
+
+  // scroll to the cursor location
+  scrollTo(visibleLines(cm).cursor, 'center');
+
+}
+
+
+///////////////////
+// LAUNCH EDITOR //
+///////////////////
+
+$(function(){
+
+  // starter text for editor
+  $('textarea#editor').text(md_test + gfm_test);
+
+  // convert textarea to CodeMirror editor
+  window.cm = CodeMirror.fromTextArea($('#editor')[0], {
+    mode:         "gfm",
+    theme:        "elegant",
+    lineNumbers:  true,
+    lineWrapping: true,
+    underscoresBreakWords: false,
+    taskLists: true,
+    fencedCodeBlocks: true,
+    strikethrough: true,
+    extraKeys:    {
+      "Ctrl-Q": function(cm){ cm.foldCode(cm.getCursor()); },
+      "Enter": "newlineAndIndentContinueMarkdownList"
+    },
+    foldGutter:   true,
+    gutters:      ["CodeMirror-linenumbers", "CodeMirror-foldgutter"]
+  });
+
+  // render starter text and re-render on text change
+  render();
+  cm.on('change', _.debounce(render, 300, {maxWait:1000})); // render when typing stops
+
+  // synchronize scrolling
+  scrollSync = _.debounce(function(a){scrollTo(visibleLines(a).top)}, 100, {maxWait:100});
+  cm.on('scroll', scrollSync);
+  cm.on('scroll', scrollSync);
+
+});
