9868a361deb67bc09504274475bfc364e082fbc7
St. Laurent, Daryl <daryl.stlaurent@stresearch.com>
2015-12-16 09:55:16 -0500

    Adding script to extract tests from marked.js

---
+++ extract-tests.py
@@ -0,0 +1,26 @@
+import os
+import re
+
+# files to read
+files = [ f for f in os.listdir('marked/test/tests') if f.endswith('.text') ]
+
+# iterate over files
+for f in files:
+
+    # base file name
+    base = f.replace('.text','')
+    
+    # extract text and html (skipping blank lines in html)
+    text = '  '.join([ x for x in open('marked/test/tests/'+base+'.text') ])
+    html = '  '.join([ x for x in open('marked/test/tests/'+base+'.html') if not re.match(r'^\s*$',x) ])
+    
+    # escape quotes in text input
+    text = text.replace('"', '&quot;')
+    
+    # generate corresponding output
+    print('<!-- ' + base + '-->')
+    print('<section id="' + base + '" data-text="')
+    print('  ' + text)
+    print('  ">')
+    print('  ' + html)
+    print('</section>')    
