6e65513d639fd523513baa240e2d459a38aa865f
St. Laurent, Daryl <daryl.stlaurent@stresearch.com>
2015-12-23 11:26:14 -0500

    Updating python script to automatically populate validation HTML file

--- extract-tests.py
+++ extract-tests.py
@@ -1,5 +1,17 @@
+#!/usr/bin/env python
+
 import os
 import re
+
+# read content from validate.html
+htmlContent = [x for x in open('validate.html')]
+
+# identify template boundaries
+preIdx  = htmlContent.index([ x for x in htmlContent if re.match(r'\s*<!-- START AUTO-POPULATED TESTS -->',x) ][0])
+postIdx = htmlContent.index([ x for x in htmlContent if re.match(r'\s*<!-- END AUTO-POPULATED TESTS -->'  ,x) ][0]) 
+
+# initialize output array with start of validate.html
+out = htmlContent[:preIdx+1]
 
 # files to read
 files = [ f for f in os.listdir('marked/test/tests') if f.endswith('.text') ]
@@ -16,16 +28,29 @@ for f in files:
     else:
 
         # extract text and html (skipping blank lines in html)
-        text = '  '.join([ x for x in open('marked/test/tests/'+base+'.text') ])
-        html = '  '.join([ x for x in open('marked/test/tests/'+base+'.html') if not re.match(r'^\s*$',x) ])
+        text = ''.join([ 
+            re.sub(r'\s+\n', '\n', '    '+x)
+            for x in open('marked/test/tests/'+base+'.text') ])
+        html = ''.join([ 
+            re.sub(r'\s+\n', '\n', '    '+x)
+            for x in open('marked/test/tests/'+base+'.html') 
+            if not re.match(r'^\s*$',x) ])
 
         # escape quotes in text input
         text = text.replace('"', '&quot;')
 
         # generate corresponding output
-        print('<!-- ' + base + '-->')
-        print('<section id="' + base + '" data-text="')
-        print('  ' + text)
-        print('  ">')
-        print('  ' + html)
-        print('</section>')
+        out.append('  <!-- ' + base + '-->\n')
+        out.append('  <section id="' + base + '" data-text="\n')
+        out.append(text + '\n')
+        out.append('    ">\n')
+        out.append(html + '\n')
+        out.append('  </section>\n')
+
+# extend output array with end of validate.html
+out.append('\n')
+out.extend(htmlContent[postIdx:])
+
+# update validate.html file with new content
+with open('validate.html', 'w') as handle:
+    handle.writelines(out)
