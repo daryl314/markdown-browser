509fbb81b0f459f3ec8f134d17ccec326ae1edfa
St. Laurent, Daryl <daryl.stlaurent@stresearch.com>
2018-08-25 07:27:12 -0400

    Adding PDF generation scripts

---
+++ script/gen_pdf.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# check input arguments
+if [[ $# -lt 1 ]]; then
+  echo "USAGE:"
+  echo "  $0 sync_directory"
+  echo "  $0 markdown_file [pdf_file]"
+  exit 1
+fi
+
+# absolute path to script location
+LOC=$(cd "$(dirname "$0")"; pwd)
+
+# process a file
+if [[ -f $1 ]]; then
+  if [[ $# -lt 2 ]]; then
+    OUT_FILE=$1.pdf
+  else
+    OUT_FILE=$2
+  fi
+  echo "$(basename "$1") --> $(basename "$OUT_FILE")"
+  cat "$1" | $LOC/md_to_pandoc.py \
+    | pandoc -f markdown -o "$OUT_FILE" -V geometry:margin=0.5in
+
+# process a directory
+elif [[ -d $1 ]]; then
+  echo "Processing directory: $1"
+  find $1 -name "*.md" -exec $LOC/$(basename $0) "{}" \;
+
+# otherwise something isn't right
+else
+  echo "Unrecognized input: $1"
+  exit 1
+fi
---
+++ script/md_to_pandoc.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+import sys, re
+
+# read data from stdin
+txt = sys.stdin.read()
+
+# escape inline latex
+#txt = re.sub(r'(?m)\$\$\s*([\s\S]*?)\s*\$\$', r'$$\1$$', txt)
+txt = re.sub(r'(?m)\$\$\s*([\s\S]*?)\s*\$\$', r'$\1$', txt)
+
+# escape block latex
+txt = re.sub(r'\\\\\(\s*([\s\S]*?)\s*\\\\\)', r'$$\1$$', txt)
+
+# remove TOC entries
+txt = re.sub(r'\[TOC\]\s*', '', txt)
+
+# dump data back to stdout
+sys.stdout.write(txt)
