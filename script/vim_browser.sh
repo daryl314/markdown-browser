1bba2feb9d9b756bb6adf1dba3aea935c9c343e2
Daryl St. Laurent <daryl.stlaurent@stresearch.com>
2020-04-03 16:09:02 -0400

    vim_browser updates

--- script/vim_browser.sh
+++ script/vim_browser.sh
@@ -1,25 +1,38 @@
 #!/bin/bash
+set -x
 
 # location of script directory
 SCRIPT=$(cd "$(dirname "$0")"; pwd)
 
+# open a file
+function open_file() {
+    if [[ $# -eq 2 ]]; then
+        local ARGS=(-c "call ScrollToHeadingText('$2')" -c 'cmap q qa')
+    else
+        local ARGS=(-c 'cmap q qa')
+    fi
+    if echo $1 2>&1 | grep '\.md$' >/dev/null; then
+        # markdown file that needs to be fed through conversion tool
+        $SCRIPT/MarkdownTool.py "$1" --action JSON \
+            | PYTHONPATH=$SCRIPT/pyvim vim -R - -c "so $SCRIPT/renderer.vim" -c 'call ParseJSON()' "${ARGS[@]}"
+    else
+        # preprocessed json
+        PYTHONPATH=$SCRIPT/pyvim vim -c "so $SCRIPT/renderer.vim" -c "view $1" "${ARGS[@]}"
+    fi
+}
+
 # if a file was passed, open it
 if [[ $# -ge 1 ]]; then
-
-  # if an anchor was passed, jump to it after opening the file
-  if [[ $1 == *#* ]]; then
-    FILE=`echo $1 | sed 's/#.*//' | sed 's/ /\\\\ /g'`
-    LINK=`echo $1 | sed 's/.*#//'`
-    PYTHONPATH=$SCRIPT/pyvim vim -c "so $SCRIPT/renderer.vim" -c "view $FILE" -c "call ScrollToHeadingText('$LINK')" -c "cmap q qa"
-
-  # otherwise just open the file
-  else
-    FILE=`echo $1 | sed 's/ /\\\\ /g'`
-    PYTHONPATH=$SCRIPT/pyvim vim -c "so $SCRIPT/renderer.vim" -c "view $FILE" -c "cmap q qa"
-  fi
+    if [[ $1 == *#* ]]; then  # has an anchor
+        FILE="$(echo $1 | sed 's/#.*//')"
+        LINK="$(echo $1 | sed 's/.*#//')"
+        open_file "$FILE" "$LINK"
+    else  # no anchor
+        open_file "$1"
+    fi
 
 # otherwise open root directory
 else
-  PYTHONPATH=$SCRIPT/pyvim vim -c "so $SCRIPT/renderer.vim" -c "view $SCRIPT/../" -c "cmap q qa"
+    PYTHONPATH=$SCRIPT/pyvim vim -c "so $SCRIPT/renderer.vim" -c "view $SCRIPT/../" -c "cmap q qa"
 fi
 
